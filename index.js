dcd229d7e08a6c9e8a3daafb6d690a039fe37264
Mukarram Nawaz <156927769+MukarramNawaz@users.noreply.github.com>
2024-08-03 09:59:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use (cors(
-    
-))
+app.use (cors());
 app.use(bodyParser.json());
 app.use ("", appRoutes)
 
